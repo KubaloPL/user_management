505a68818e2213187460dc2b4dc86c3ce66dc044
KubaloPL <superkubalo132@gmail.com>
2024-12-03 10:34:11 +0100

    sync

--- user_management.py
+++ user_management.py
@@ -1,35 +1,71 @@
-import 
+import json
+import random
+import re
+import os
 
-def add_user(user_data): 
+users = []
+
+def add_user(user_data: dict): 
     '''
     Dodaje nowego użytkownika.
+     {
+     "user_id": 1,
+     "name": "Jakub",
+     "nip": 12345,
+     "pesel": "59124",
+     "regon": 122345,
+     "password": "123"
+    }
     '''
 
+    users.append(user_data)
+    json.dump()
+
+def save_users_to_file(users):
+    pass
 
 def remove_user(user_id): 
     '''
     Usuwa istniejącego użytkownika.
     '''
+
+
+    
 def edit_user(user_id, updated_data): 
     '''
     Edytuje dane użytkownika.
     '''
+
+
+
 def validate_nip(nip): 
     '''
     Waliduje numer NIP.
     '''
+
+
+
 def validate_pesel(pesel): 
     '''
     Waliduje numer PESEL.
     '''
+
+
+
 def validate_regon(regon): 
     '''
     Waliduje numer REGON.
     '''
+
+
+
 def generate_password(): 
     '''
     Generuje silne hasło.
     '''
+
+
+    
 def validate_password(password): 
     '''
     Waliduje siłę hasła.
