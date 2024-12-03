ea957e9dc0afe36010fee01c5d4d165ce947682b
KubaloPL <superkubalo132@gmail.com>
2024-12-03 09:49:26 +0100

    add function names

---
+++ user_management.py
@@ -0,0 +1,33 @@
+
+def add_user(user_data): 
+    '''Dodaje nowego użytkownika.'''
+
+def remove_user(user_id): 
+    '''
+    Usuwa istniejącego użytkownika.
+    '''
+def edit_user(user_id, updated_data): 
+    '''
+    Edytuje dane użytkownika.
+    '''
+def validate_nip(nip): 
+    '''
+    Waliduje numer NIP.
+    '''
+def validate_pesel(pesel): 
+    '''
+    Waliduje numer PESEL.
+    '''
+def validate_regon(regon): 
+    '''
+    Waliduje numer REGON.
+    '''
+def generate_password(): 
+    '''
+    Generuje silne hasło.
+    '''
+def validate_password(password): 
+    '''
+    Waliduje siłę hasła.
+    '''
+
